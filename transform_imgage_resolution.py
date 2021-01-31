25186964a23b9d60b837fdb03b11c049d558a854
Nightgg42 <48233971+Nightgg42@users.noreply.github.com>
2021-01-31 20:25:42 +0700

    Add files via upload

---
+++ transform_imgage_resolution.py
@@ -0,0 +1,18 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:37:50) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from PIL import Image
+import os
+import argparse
+
+def rescale_images(directory, size):
+    for img in os.listdir(directory):
+        im = Image.open(directory+img)
+        im_resized = im.resize(size, Image.ANTIALIAS)
+        im_resized.save(directory+img)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Rescale images")
+    parser.add_argument('-d', '--directory', type=str, required=True, help='Directory containing the images')
+    parser.add_argument('-s', '--size', type=int, nargs=2, required=True, metavar=('width', 'height'), help='Image size')
+    args = parser.parse_args()
+    rescale_images(args.directory, args.size)
